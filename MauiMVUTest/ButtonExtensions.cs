cfc3ac4db451b89ca161579e06c1717b5e6bd526
Stephen Quan <squan@esri.com>
2025-05-19 16:02:52 +1000

    Add some example extension methods

---
+++ MauiMVUTest/ButtonExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiMVUTest;
+public static class ButtonExtensions
+{
+	public static Button Text(this Button button, string text)
+	{
+		button.Text = text;
+		return button;
+	}
+
+	public static Button Command(this Button button, Action action)
+	{
+		button.Command = new Command(action);
+		return button;
+	}
+}
---
+++ MauiMVUTest/LabelExtensions.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Maui.Markup;
+
+namespace MauiMVUTest;
+public static class LabelExtensions
+{
+	public static Label BindText(this Label label, string propertyName)
+	{
+		label.Bind(Label.TextProperty, propertyName);
+		return label;
+	}
+}
