8d4d0834fa4fa1a2dcb113a5cec4c58f5928a61d
Stephen Quan <squan@esri.com>
2025-05-20 12:54:37 +1000

    Reduce extension method usage to simplify commercial component story

--- MauiMVUTest/MainPage.cs
+++ MauiMVUTest/MainPage.cs
@@ -2,17 +2,16 @@
 using CommunityToolkit.Maui.Markup;
 namespace MauiMVUTest;
 
-/// <summary>
-/// This is the main page of the application.
-/// </summary>
+/// <summary>This is the main page of the application.</summary>
 public partial class MainPage : ContentPage
 {
+    /// <summary>The number of times the button has been clicked.</summary>
     [BindableProperty] public partial int ClickCount { get; private set; } = 0;
+
+    /// <summary>The text displayed on the label.</summary>
     [BindableProperty] public partial string MyText { get; private set; } = "Hello, World!";
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MainPage"/> class.
-    /// </summary>
+    /// <summary>Initializes a new instance of the <see cref="MainPage"/> class.</summary>
     public MainPage()
     {
         BindingContext = this;
@@ -23,16 +22,20 @@ public partial class MainPage : ContentPage
                 Padding = new Thickness(25, 25, 25, 25),
                 Children =
                 {
-                    new Label() { }
+                    new Label()
+                    {
+                    }
                         .Bind(Label.TextProperty, (MainPage ctx) => ctx.MyText),
-                    new Button() { }
-                        .BorderColor(Colors.Green)
-                        .BorderWidth(1.0)
-                        .BackgroundColor(Colors.White)
-                        .CornerRadius(5)
-                        .Text("Update Text")
-                        .TextColor(Colors.Green)
-                        .Command(() => { MyText = $"Clicked {++ClickCount} times"; })
+                    new Button()
+                    {
+                        BorderColor = Colors.Green,
+                        BorderWidth = 1.0,
+                        BackgroundColor = Colors.White,
+                        CornerRadius = 5,
+                        Text = "Update Text",
+                        TextColor = Colors.Green,
+                        Command = new Command(() => { MyText = $"Clicked {++ClickCount} times"; })
+                    }
                 }
             };
     }
